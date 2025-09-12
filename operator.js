6a90b2e8b9853130534a03559364d4c66f182668
Divyanshu <divyanshu.gairwal2004@gmail.com>
2025-09-12 19:03:32 +0530

    Create operator.js

---
+++ operator.js
@@ -0,0 +1,12 @@
+// A function to demonstrate operator usage
+function operate(a, b) {
+  console.log("Addition (a + b):", a + b);
+  console.log("Subtraction (a - b):", a - b);
+  console.log("Multiplication (a * b):", a * b);
+  console.log("Division (a / b):", a / b);
+  console.log("Modulus (a % b):", a % b);
+  console.log("Exponent (a ** b):", a ** b);
+}
+
+// Call the function
+operate(10, 3);
