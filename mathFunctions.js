023c7bc6b8a5e8e86abcf38dcbb4739d239db837
Divyanshu <divyanshu.gairwal2004@gmail.com>
2025-09-15 19:36:28 +0530

    Create mathFunctions.js

---
+++ mathFunctions.js
@@ -0,0 +1,12 @@
+// A simple function to multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
+
+// A simple function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Export the functions so they can be used in other files
+module.exports = { multiply, add };
